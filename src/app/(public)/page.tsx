5f8921b7a1c942333868ebc45fde80ffe73f78c6
TheCatLady <52870424+TheCatLady@users.noreply.github.com>
2024-09-19 19:12:38 -0700

    fix: featured page list styling

--- src/app/(public)/page.tsx
+++ src/app/(public)/page.tsx
@@ -30,7 +30,7 @@ export default async function HomePage() {
           <h2 id="pages-heading" className="sr-only">
             Pages
           </h2>
-          <div className="mx-auto grid max-w-4xl grid-cols-1 gap-y-12 lg:grid-cols-2 lg:gap-x-8 lg:gap-y-0">
+          <div className="mx-auto grid max-w-4xl grid-cols-1 gap-6 lg:grid-cols-2 lg:gap-8">
             {featuredPages?.map((page) => {
               const Icon = page.icon
                 ? dynamic(() =>
@@ -50,8 +50,8 @@ export default async function HomePage() {
                   className="group relative mx-auto max-w-xl rounded-lg border border-gray-300 bg-white px-5 py-4 text-center shadow-sm focus-within:ring-2 focus-within:ring-cyan-500 focus-within:ring-offset-2 hover:border-gray-400 md:flex md:items-start md:text-left"
                 >
                   <div className="md:flex-shrink-0">
-                    <div className="mx-auto flex h-16 w-16 items-center justify-center rounded-full bg-cyan-50/50 group-hover:bg-cyan-50/75 md:h-20 md:w-20">
-                      <Icon className="h-8 w-8 text-cyan-700 group-hover:text-cyan-800 md:h-12 md:w-12" />
+                    <div className="mx-auto flex h-16 w-16 items-center justify-center rounded-full bg-cyan-50/50 group-hover:bg-cyan-50/75">
+                      <Icon className="h-8 w-8 text-cyan-700 group-hover:text-cyan-800" />
                     </div>
                   </div>
                   <div className="mt-3 md:ml-4 md:mt-0">
