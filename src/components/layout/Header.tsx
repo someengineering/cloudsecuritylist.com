b1dc58f47ca7e00aba7c291e73fa614504d7e2d7
TheCatLady <52870424+TheCatLady@users.noreply.github.com>
2024-09-25 13:17:28 -0700

    fix: navigation menu

--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -24,12 +24,12 @@ export default function Header({
         aria-label="Global"
         className="mx-auto flex max-w-7xl items-center justify-between p-6 lg:px-8"
       >
-        <Link href="/" className="-m-1.5 flex flex-shrink-0 items-center p-1.5">
+        <Link
+          href="/"
+          className="-m-1.5 flex flex-shrink-0 items-center p-1.5 text-cyan-600 hover:text-cyan-700 focus:outline-none"
+        >
           <span className="sr-only">{title}</span>
-          <Logo
-            className="h-9 w-auto text-cyan-600 hover:text-cyan-700 focus:outline-none sm:h-10"
-            aria-hidden="true"
-          />
+          <Logo className="h-9 w-auto sm:h-10" aria-hidden="true" />
         </Link>
         <div className="hidden gap-x-12 lg:flex">
           {navigation?.map((item) => (
@@ -63,15 +63,16 @@ export default function Header({
         onClose={setMobileMenuOpen}
         className="lg:hidden"
       >
-        <div className="fixed inset-0 z-10" />
-        <DialogPanel className="fixed inset-y-0 right-0 z-10 w-full overflow-y-auto bg-white p-6 sm:max-w-sm sm:ring-1 sm:ring-gray-900/10">
+        <div className="fixed inset-0 z-50 bg-gray-500/50" />
+        <DialogPanel className="fixed inset-y-0 right-0 z-50 w-full overflow-y-auto bg-white p-6 sm:max-w-sm sm:ring-1 sm:ring-gray-900/10">
           <div className="flex items-center justify-between">
             <Link
               href="/"
-              className="-m-1.5 p-1.5 text-xl font-bold"
+              className="-m-1.5 p-1.5 text-cyan-600 hover:text-cyan-700 focus:outline-none"
               onClick={() => setMobileMenuOpen(false)}
             >
-              {title}
+              <span className="sr-only">{title}</span>
+              <Logo className="h-9 w-auto sm:h-10" aria-hidden="true" />
             </Link>
             <button
               type="button"
