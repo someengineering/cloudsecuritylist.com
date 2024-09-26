dad248f7bec557ff109b53c148d7220b7f1b9afa
TheCatLady <52870424+TheCatLady@users.noreply.github.com>
2024-09-26 11:52:30 -0700

    fix: icons

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,8 +1,29 @@
-import { Viewport } from 'next';
+import { Metadata, Viewport } from 'next';
 
 export const viewport: Viewport = {
   themeColor: '#0891b2',
   colorScheme: 'only light',
+};
+
+export const metadata: Metadata = {
+  icons: [
+    {
+      url: '/favicon.ico',
+      type: 'image/x-icon',
+      sizes: '16x16 32x32',
+    },
+    {
+      url: '/icon.svg',
+      type: 'image/svg+xml',
+      sizes: 'any',
+    },
+    {
+      rel: 'apple-touch-icon',
+      url: '/apple-touch-icon.png',
+      type: 'image/png',
+      sizes: '180x180',
+    },
+  ],
 };
 
 export default function RootLayout({
