e0cc59d95f63790df60a148df9143cd928205aa5
TheCatLady <52870424+TheCatLady@users.noreply.github.com>
2024-09-26 16:47:15 -0700

    fix: tweak OpenGraph images

--- src/components/opengraph/Layout.tsx
+++ src/components/opengraph/Layout.tsx
@@ -10,6 +10,13 @@ export default function Layout({ children }: { children: ReactElement }) {
           'linear-gradient(165deg, #0891b2, #0891b2 75%, #46adc6 75%, #46adc6)',
       }}
     >
+      <div
+        tw="absolute inset-0 top-0 h-full w-full opacity-15"
+        style={{
+          backgroundImage:
+            'linear-gradient(100deg, transparent, transparent 90%, #fff 90%, #fff)',
+        }}
+      />
       <div tw="flex p-8">
         <Logo tw="text-cyan-600" width={336} height={49} />
       </div>
