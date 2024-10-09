1703261fc7d92768d2acf83f7d4804a3465b4089
TheCatLady <52870424+TheCatLady@users.noreply.github.com>
2024-10-08 22:43:13 -0700

    refactor: remove no-longer-needed logic from sitemap generation (#3)

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -4,12 +4,5 @@ import { MetadataRoute } from 'next';
 export default async function sitemap(): Promise<MetadataRoute.Sitemap> {
   const sitemap = await getSitemap();
 
-  if (!sitemap) {
-    return [];
-  }
-
-  return sitemap.map((loc) => ({
-    url: loc.url,
-    lastModified: loc.lastModified ? new Date(loc.lastModified) : undefined,
-  }));
+  return sitemap ?? [];
 }
